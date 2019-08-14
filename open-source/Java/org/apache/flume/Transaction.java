8dda196e08d5e2a03fe59c4eb7ae6c323a8eef78
zozospider <41692211+zozospider@users.noreply.github.com>
2019-08-14 13:55:35 +0800

    Update Transaction.java

--- open-source/Java/org/apache/flume/Transaction.java
+++ open-source/Java/org/apache/flume/Transaction.java
@@ -62,9 +62,13 @@ public interface Transaction {
    * <p>Starts a transaction boundary for the current channel operation. If a
    * transaction is already in progress, this method will join that transaction
    * using reference counting.</p>
+   * <p>启动当前 Channel 操作的 Transaction 边界. 如果 Transaction 已经在进行中, 该方法将使用引用计数加入该 Transaction 中.</p>
    * <p><strong>Note</strong>: For every invocation of this method there must
    * be a corresponding invocation of {@linkplain #close()} method. Failure
    * to ensure this can lead to dangling transactions and unpredictable results.
+   * </p>
+   * <p>
+   * <strong>注意</strong>: 每次调用该方法, 必须有相应的 {@linkplain #close()} 方法调用. 未能确保这一点可能会导致 Transaction 悬空和不可预知的结果.
    * </p>
    */
   void begin();
@@ -72,12 +76,14 @@ public interface Transaction {
   /**
    * Indicates that the transaction can be successfully committed. It is
    * required that a transaction be in progress when this method is invoked.
+   * 表示可以成功提交 Transaction. 调用此方法时, 要求 Transaction 正在进行中.
    */
   void commit();
 
   /**
    * Indicates that the transaction can must be aborted. It is
    * required that a transaction be in progress when this method is invoked.
+   * 表示必须终止 Transaction. 调用此方法时, 要求 Transaction 正在进行中.
    */
   void rollback();
 
@@ -86,9 +92,15 @@ public interface Transaction {
    * transaction is already in progress, this method will join that transaction
    * using reference counting. The transaction is completed only if there
    * are no more references left for this transaction.</p>
+   * <p>
+   * 结束当前 Channel 操作的 Transaction 边界. 如果 Transaction 正在进行中, 则此方法将使用引用计数加入该事物. 仅当没有为此 Transaction 保留的引用时, Transaction 才会完成.
+   * </p>
    * <p><strong>Note</strong>: For every invocation of this method there must
    * be a corresponding invocation of {@linkplain #begin()} method. Failure
    * to ensure this can lead to dangling transactions and unpredictable results.
+   * </p>
+   * <p>
+   * <strong>注意</strong>: 每次调用该方法, 必须有相应的 {@linkplain #begin()} 方法调用. 未能确保这一点可能会导致 Transaction 悬空和不可预知的结果.
    * </p>
    */
   void close();
