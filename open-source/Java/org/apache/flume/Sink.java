436369c9a51b95df0c69cbb6212bec5471080616
zozospider <41692211+zozospider@users.noreply.github.com>
2019-09-19 18:35:16 +0800

    Update Sink.java

--- open-source/Java/org/apache/flume/Sink.java
+++ open-source/Java/org/apache/flume/Sink.java
@@ -58,24 +58,31 @@ import org.apache.flume.lifecycle.LifecycleAware;
 public interface Sink extends LifecycleAware, NamedComponent {
   /**
    * <p>Sets the channel the sink will consume from</p>
+   * <p>设置 Sink 将使用的 Channel<p>
    * @param channel The channel to be polled
+   * @param channel 要轮询的 Channel
    */
   public void setChannel(Channel channel);
 
   /**
    * @return the channel associated with this sink
+   * @return 与此 Sink 关联的 Channel
    */
   public Channel getChannel();
 
   /**
    * <p>Requests the sink to attempt to consume data from attached channel</p>
+   * <p>请求 Sink 尝试消费所属 Channel 中的数据</p>
    * <p><strong>Note</strong>: This method should be consuming from the channel
    * within the bounds of a Transaction. On successful delivery, the transaction
    * should be committed, and on failure it should be rolled back.
+   * <p><string>注意</strong>: 此方法应使用 Transaction 范围内的 Channel. 在成功传送时, Transaction 应该被提交, 并且在失败时应该被回滚.
    * @return READY if 1 or more Events were successfully delivered, BACKOFF if
    * no data could be retrieved from the channel feeding this sink
+   * @return 如果成功传送了 1 个或多个 Events, 则 READY, 如果无法从提供此 Sink 的 Channel 中检索到数据, 则 BACKOFF
    * @throws EventDeliveryException In case of any kind of failure to
    * deliver data to the next hop destination.
+   * @throws EventDeliveryException 在任何类型的无法将数据传递到下一个跃点目的地的情况下.
    */
   public Status process() throws EventDeliveryException;
 
